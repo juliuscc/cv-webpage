277fdcd3fb691fbed11b2230f89362c67b7a9f76
jcelik <julius.cc@hotmail.com>
2019-02-11 19:03:50 +0100

    added files

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+const ImageCard = () => <div />
+
+class index extends Component {
+	render() {
+		return (
+			<>
+				<header />
+				<ImageCard />
+			</>
+		)
+	}
+}
+
+export default index
