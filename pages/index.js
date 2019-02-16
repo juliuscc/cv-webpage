bb85b0d5f2f898c4824de03a4e5227f5c37f81d4
jcelik <julius.cc@hotmail.com>
2019-02-16 17:20:43 +0100

    Fixed experience-list

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const TitleBar = styled.div`
 	}
 `
 
-import { Experiences } from '../components/Experiences'
+import { Experiences as ExperienceList } from '../components/Experiences'
 class index extends Component {
 	state = {
 		selectedSection: 0,
@@ -33,9 +33,34 @@ class index extends Component {
 							'A student consultant company focusing on web and app development.',
 
 						start: 'February 2019'
+					},
+					{
+						title: 'Systems developer',
+						company: 'LS Elektronik AB',
+						description:
+							'LS Elektronik AB is an engineering and manufacturing company. They have a broad range of products with a focus on radio technology and radio over IP.',
+						start: 'October 2016',
+						end: 'August 2018'
+					},
+					{
+						title: 'Bachelor Thesis',
+						company: 'LS Elektronik AB',
+						description:
+							'As a part of my education at the KTH Royal Institute of Technology, a bachelor thesis was conducted at LS Elektronik. The thesis was conducted as a case study that examined a large set of existing tools that use JSON Schemas to generate graphical user interfaces. The thesis also present a new kind  of tool that use JSON Schemas to generate a user input form to manipulate the data described and annotated by a given JSON Schema.',
+						start: 'March 2018',
+						end: 'June 2018'
+					},
+					{
+						title: 'Study coach',
+						company: 'My Academy',
+						description:
+							'I helped young students reach their goals by teaching them mathematics on secondary school level.',
+						start: 'January 2016',
+						end: 'June 2016'
 					}
 				]
-			}
+			},
+			{ title: 'Education', items: [] }
 		]
 	}
 
@@ -46,7 +71,11 @@ class index extends Component {
 					<SectionTitle>Experiences</SectionTitle>
 					<SectionTitle>Experiences</SectionTitle>
 				</TitleBar>
-				<Experiences />
+				<ExperienceList
+					items={
+						this.state.sections[this.state.selectedSection].items
+					}
+				/>
 				<div style={{ height: '100px' }} />
 			</Container>
 		)
