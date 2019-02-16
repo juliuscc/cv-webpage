84f7b473ab8121548868aa17c168b6bbd3b8c113
jcelik <julius.cc@hotmail.com>
2019-02-16 21:25:57 +0100

    refactored out data

--- pages/index.js
+++ pages/index.js
@@ -2,51 +2,18 @@ import React, { Component } from 'react'
 import Container from '../components/styles/Container'
 import { Experiences as ExperienceList } from '../components/Experiences'
 import { NavBar, SectionTitle } from '../components/styles/NavBar'
+import data from '../data/data'
 
 class index extends Component {
+	constructor(props) {
+		super(props)
+
+		console.log(data)
+	}
+
 	state = {
 		selectedSection: 0,
-		sections: [
-			{
-				title: 'Experiences',
-				items: [
-					{
-						title: 'IT Consultant',
-						company: 'We Know IT',
-						description:
-							'A student consultant company focusing on web and app development.',
-
-						start: 'February 2019'
-					},
-					{
-						title: 'Systems developer',
-						company: 'LS Elektronik AB',
-						description:
-							'LS Elektronik AB is an engineering and manufacturing company. They have a broad range of products with a focus on radio technology and radio over IP.',
-						start: 'October 2016',
-						end: 'August 2018'
-					},
-					{
-						title: 'Bachelor Thesis',
-						company: 'LS Elektronik AB',
-						description:
-							'As a part of my education at the KTH Royal Institute of Technology, a bachelor thesis was conducted at LS Elektronik. The thesis was conducted as a case study that examined a large set of existing tools that use JSON Schemas to generate graphical user interfaces. The thesis also present a new kind  of tool that use JSON Schemas to generate a user input form to manipulate the data described and annotated by a given JSON Schema.',
-						start: 'March 2018',
-						end: 'June 2018'
-					},
-					{
-						title: 'Study coach',
-						company: 'My Academy',
-						description:
-							'I helped young students reach their goals by teaching them mathematics on secondary school level.',
-						start: 'January 2016',
-						end: 'June 2016'
-					}
-				]
-			},
-			{ title: 'Education', items: [] },
-			{ title: 'Projects', items: [] }
-		]
+		sections: data
 	}
 
 	render() {
